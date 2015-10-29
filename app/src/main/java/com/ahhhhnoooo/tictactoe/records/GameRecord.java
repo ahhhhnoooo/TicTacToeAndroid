912cd85f0a4e613fa704a7a5db39f5318878e701
Ben Ng <ahhhhnoooo@gmail.com>
2015-10-29 14:14:34 -0700

    Add game records database functionality

---
+++ app/src/main/java/com/ahhhhnoooo/tictactoe/records/GameRecord.java
@@ -0,0 +1,7 @@
+package com.ahhhhnoooo.tictactoe.records;
+
+/**
+ * Created by ahhhh on 10/28/2015.
+ */
+public class GameRecord {
+}
---
+++ app/src/main/java/com/ahhhhnoooo/tictactoe/records/GameRecordsSaveTask.java
@@ -0,0 +1,7 @@
+package com.ahhhhnoooo.tictactoe.records;
+
+/**
+ * Created by ahhhh on 10/27/2015.
+ */
+public class GameRecordsSaveTask {
+}
